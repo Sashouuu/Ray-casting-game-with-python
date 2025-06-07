655787a440e71ee7adebd9fb17579bfd07c82af2
Sashouuu <darkwasp4@gmail.com>
2025-06-08 02:36:24 +0300

    Update demo.py
    Deleted everything and started from scratch due to memory optimization issues.

--- demo.py
+++ demo.py
@@ -1,90 +1,64 @@
+import pygame as pg
 import numpy as np
-from matplotlib import pyplot as plt
-import keyboard
 
-size = 15
-mapa = [[list(np.random.uniform(0, 1, 3))] * size for i in range(size)]
-for i in range(size - 2):
-    for j in range(size - 2):
-        if np.random.uniform() > 0.33:
-            mapa[i+1][j+1] = 0
 
-posx, posy = (1, np.random.randint(1, size - 1))
-rot = np.pi / 4  # initial rotation, indicating where player is looking at
+def main():
+    pg.init()
+    screen = pg.display.set_mode((800, 600))  # sets game screen size
+    is_running = True  # if True game is running
+    clock = pg.time.Clock()
 
-x, y = posx, posy
-mapa[x][y] = 0
+    hres = 120  # horizontal resolution
+    halfvres = 100  # vetcial resolution / 2
+    mod = hres / 60  # scaling factor (60 fov)
 
-count = 0
-while True:
-    testx, testy = x, y
-    if np.random.uniform() > 0.5:
-        testx = testx + np.random.choice([-1, 1])
-    else:
-        testy = testy + np.random.choice([-1, 1])
-    if 0 < testx < size - 1 and 0 < testy < size - 1:
-        if mapa[testx][testy] == 0 or count > 5:
-            count = 0
-            x, y = testx, testy
-            mapa[x][y] = 0
-        if x == size - 2:
-            exitx, exity = x, y
-            break
-        else:
-            count += 1
+    posx, posy, rot = 0, 0, 0
+    frame = np.random.uniform(0, 1, (hres, halfvres * 2, 3))
 
-while True:
+    while is_running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                is_running = False  # if the close button is pressed the game stops
 
-    plt.hlines(-0.6, 0, 60, colors="gray", lw=165, alpha=0.5)
-    plt.hlines(0.6, 0, 60, colors="lightblue", lw=165, alpha=0.5)
-    tilex, tiley, tilec = ([], [], [])
+        for i in range(hres):
+            rot_i = rot + np.deg2rad(i / mod - 30)
+            sin, cos = np.sin(rot_i), np.cos(rot_i)
 
-    for i in range(60): #FOV = 60 degrees
-        rot_i = rot + np.deg2rad(i - 30)  #varying viewing angle
-        x, y = posx, posy
-        sin, cos = 0.02 * np.sin(rot_i), 0.02 * np.cos(rot_i)
-        n = 0
-        while True:
-            xx, yy = x, y
-            x, y = x + cos, y + sin
-            n += 1
-            if abs(int(3 * xx) - int(3 * x)) > 0 or abs(int(3 * yy) - int(3 * y)) > 0:
-                tilex.append(i)
-                tiley.append(-1 / (0.02 * n))
-                if int(x) == exitx and int(y) == exity:
-                    tilec.append("b")
+            for j in range(halfvres):
+                n = halfvres / (halfvres - j)  # distance from floor to player
+                x, y = posx + cos * n, posy + sin * n  # calculating position of each pixel
+
+                if int(x) % 2 == int(y) % 2:
+                    frame[i][halfvres * 2 - j - 1] = [0, 0, 0]
                 else:
-                    tilec.append("k")
+                    frame[i][halfvres * 2 - j - 1] = [1, 1, 1]
 
-            if mapa[int(x)][int(y)] != 0:
-                h = np.clip(1 / (0.02 * n), 0, 1)
-                c = np.asarray(mapa[int(x)][int(y)]) * (0.3 + 0.7 * h ** 2)
-                break
-        plt.vlines(i, -h, h, lw=8, colors=c)  # lw=8 - increases lines thickness
-    plt.axis("off")  # hides axis
-    plt.tight_layout()  # remove blank spaces
-    plt.axis([0, 60, -1, 1])  # limit plot region
-    plt.draw()
-    plt.pause(0.0001)  # between seperate plots there should be a short pause
-    plt.clf()  # clear old plot data
+        # transforming frame into a surface so it can be displayed
+        surf = pg.surfarray.make_surface(frame * 255)
+        surf = pg.transform.scale(surf, (800, 600))
 
-    key = keyboard.read_key() # player movement
-    x, y = posx, posy
+        screen.blit(surf, (0, 0))  # placing surface in the top left corner of game screen
+        pg.display.update()
 
-    if key == "up":
-        x, y = x + 0.3 * np.cos(rot), y + 0.3 * np.sin(rot)
-    elif key == "down":
-        x, y = x - 0.3 * np.cos(rot), y - 0.3 * np.sin(rot)
-    elif key == "left":
-        rot -= np.pi / 8
-    elif key == "right":
-        rot += np.pi / 8
-    elif key == "esc":
-        break
+        posx, posy, rot = movement(posx, posy, rot, pg.key.get_pressed())
 
-    if mapa[int(x)][int(y)] == 0:
-        if int(posx) == exitx and int(posy) == exity:
-            break
-        posx, posy = x, y
 
-plt.close() # automatically close window when exiting game
+def movement(posx, posy, rot, keys):
+    if keys[pg.K_LEFT] or keys[ord("a")]:
+        rot -= 0.1
+
+    if keys[pg.K_RIGHT] or keys[ord("d")]:
+        rot += 0.1
+
+    if keys[pg.K_UP] or keys[ord("w")]:
+        posx, posy = posx + np.cos(rot) * 0.1, posy + np.sin(rot) * 0.1
+
+    if keys[pg.K_DOWN] or keys[ord("s")]:
+        posx, posy = posx - np.cos(rot) * 0.1, posy - np.sin(rot) * 0.1
+
+    return posx, posy, rot
+
+
+if __name__ == "__main__":
+    main()
+    pg.quit()
